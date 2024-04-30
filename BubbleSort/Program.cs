1fcd4772bb5ce4f2133f6fa2a2ae1c4e779170c9
Kpoxu4 <159006608+Kpoxu4@users.noreply.github.com>
2024-04-30 11:08:03 +0300

    Add project files.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,21 @@
+﻿using Sort;
+
+
+var rnd =new Random();
+
+int [] ints = new int[10];
+for (int i = 0; i < ints.Length; i++)
+    ints[i] = rnd.Next(0, 100);   
+
+foreach (int i in ints) Console.Write(i + " ");
+
+// Сортировка пузырьком
+//var resultBubbleSorting = Sort<int>.BubbleSorting(ints);
+// Шейкерная сортировка
+var resultShakerSorting = Sort<int>.ShakerSorting(ints);
+
+Console.WriteLine();
+
+foreach (int i in resultShakerSorting) Console.Write(i + " ");
+
+
---
+++ BubbleSort/Sort.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+
+namespace Sort
+{
+    public  static class Sort <T>  where T : IComparable<T>
+    {
+        // Сортировка пузырьком
+        public static T[] BubbleSorting(T[] array)
+        {
+           
+            for (int i = 0; i < array.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                        Swop(ref array[j], ref array[j + 1]);
+                        count++;
+                    }
+                }
+                if (count == 0) break;
+            }
+            return array;
+        }
+
+        // Сортировка шейкерная
+        public static T[] ShakerSorting(T[] array)
+        {
+            int start = 0, end = array.Length - 1;
+
+            while (start <= end)
+            {
+                int count = 0;
+                for (int i = start; i < end; i++)
+                {
+                    T first = array[i];
+                    T second = array[i + 1];
+                    if (first.CompareTo(second) > 0)
+                    {
+                        Swop(ref array[i], ref array[i + 1]);
+                        count++; 
+                    }
+                   
+                }
+                if (count == 0) break;
+                else end--;
+
+
+                for (int i = end; i > start; i--)
+                {
+                    count = 0;
+                    T first = array[i];
+                    T second = array[i - 1];
+                    if (first.CompareTo(second) < 0)
+                    {
+                        Swop(ref array[i], ref array[i - 1]);
+                        count++;
+                    }
+                }
+                if (count == 0) break;
+                else start++;
+
+            }
+            return array;
+        }
+
+        private static void Swop( ref T item, ref T itemNext)
+        {
+            T helpItem = item;
+            item = itemNext;
+            itemNext = helpItem;
+        }
+    }
+}
